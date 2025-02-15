d43066944d7152e7148f2c52c4ba0c7e24688bdd
Sar2580P <sporwal1818@gmail.com>
2025-02-15 19:37:08 +0530

    improved readability and applicability of any blur(gaussian or other)

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from pipeline_seg import StableDiffusionXLSEGPipeline
+from diffusion_utils.pipeline_seg import StableDiffusionXLSEGPipeline
 import torch
-from utils import read_yaml
 import os
-from typing import List
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-import textwrap
+from omegaconf import OmegaConf
 
 try:
     pipe = StableDiffusionXLSEGPipeline.from_pretrained(
@@ -18,98 +16,47 @@ try:
 except Exception as e:
     print(f"Error: {e}")
 
-
-def create_plot(images, titles, rows, cols, save_path):
-    fig, axes = plt.subplots(rows, cols, figsize=(cols * 3, rows * 3), constrained_layout=True)
-    axes = axes.flatten() if rows * cols > 1 else [axes]  # Flatten axes for easy iteration
-
-    for i, ax in enumerate(axes):
-        if i < len(images):
-            ax.imshow(images[i])
-            ax.axis("off")
-
-            # Wrap text to avoid horizontal overlap
-            wrapped_title = textwrap.fill(titles[i], width=30)  # Adjust width for better readability
-
-            # Place title as text below the image
-            ax.text(0.5, -0.1, wrapped_title, fontsize=9, ha="center", va="top", transform=ax.transAxes, wrap=True)
-        else:
-            ax.axis("off")  # Hide extra subplots if images are fewer than grid cells
-
-    # Adjust layout to prevent overlapping
-    plt.subplots_adjust(top=0.95, bottom=0.05, wspace=0.3, hspace=0.6)  # Increase wspace & hspace
-    plt.savefig(save_path, bbox_inches="tight", dpi=300)  # Save with proper bounding box
-    plt.close()
-
-config = {
-'seed' : 4,
-'num_inference_steps' : 30,
-'seg_applied_layers' : ['down' , 'mid'],
-'guidance_scales' : [0 , 5],
-'blur_time_regions' : ['begin', 'mid'],
-'seg_scales' : [0, 3],
-'seg_blur_sigmas' : [0, 1 ,10, 10000],
-'pics_save_dir' : 'results/pics',
-'save_attention_maps' :  False,
-'atten_save_dir' : 'results/attn_maps',
-'sample_ct_attn_maps' :  3,
-'should_log_metrics' : True,
-'metric_save_dir' : 'results/metrics',
-}
-
-_config = read_yaml("config.yaml")
-prompts = [
-"a jellyfish playing the drums in an underwater concert",
-]
-config.update(_config)
-
-PICS_SAVE_DIR = os.path.join(config['pics_save_dir'], f"seed-{config['seed']}" ,
-                         f"blur_regions-({'_'.join(config['blur_time_regions'])})",
-                         f"seg_applied_layers-({'_'.join(config['seg_applied_layers'])})")
-ATTN_SAVE_DIR = os.path.join(config['atten_save_dir'], f"seed-{config['seed']}" ,
-                         f"blur_regions-({'_'.join(config['blur_time_regions'])})",
-                         f"seg_applied_layers-({'_'.join(config['seg_applied_layers'])})")
-os.makedirs(PICS_SAVE_DIR, exist_ok=True)
-os.makedirs(ATTN_SAVE_DIR, exist_ok=True)
+conf = OmegaConf.load('config.yaml')
+config = OmegaConf.create(OmegaConf.to_yaml(conf, resolve=True))
+    
+os.makedirs(config['pics_save_dir'], exist_ok=True)
+os.makedirs(config['atten_save_dir'], exist_ok=True)
+os.makedirs(config['metric_save_dir'], exist_ok=True)
 
 
 
 if __name__=="__main__":
-    for prompt in prompts:
-        for guidance_scale in tqdm(config['guidance_scales'], desc="guidance_scales"):
-            outputs =[]
-            titles = []
-            for seg_scale in config['seg_scales']:
-                for seg_blur_sigma in config['seg_blur_sigmas']:
-                    if seg_blur_sigma==0 and seg_scale>0:   # invalid case
-                        continue
-                    if guidance_scale==0:
-                        prompt = ""
+    
+    if config['guidance_scale']==0:
+        config['cfg_prompts'] = [""]  # only one prompt for guidance_scale=0
 
-                    titles.append(f"seg_blur_sigma-{seg_blur_sigma}_seg_scale-{seg_scale}_guidance_scale-{guidance_scale}")
+    for idx, (prompt) in tqdm(enumerate(config['cfg_prompts']), desc = "Generating images for prompts..."):
+        name = f"segScale-{config['seg_scale']}_guidanceScale-{config['guidance_scale']}/steps-{config['num_inference_steps']}_" + \
+                (f"prompt-{idx}" if config['guidance_scale']>0 else "")
 
-                    generator = torch.Generator(device="cuda").manual_seed(config["seed"])
-                    # generator.seed()  # This will REPLACE the manually set seed with a new random one!
+        generator = torch.Generator(device="cuda").manual_seed(config["seed"])
+        # generator.seed()  # This will REPLACE the manually set seed with a new random one!
 
-                    outputs += pipe(
-                            [prompt],
-                            num_inference_steps=config['num_inference_steps'],
-                            guidance_scale=guidance_scale,
-                            should_log_metrics = config['should_log_metrics'],
-                            metric_tracked_block = config['metric_tracked_block'],
-                            metric_save_dir = os.path.join(config['metric_save_dir'] , f"seed-{config['seed']}"),
-                            seg_scale=seg_scale,
-                            seg_blur_sigma=seg_blur_sigma,
-                            seg_applied_layers=config['seg_applied_layers'],
-                            blur_time_regions=config['blur_time_regions'],
-                            generator=generator,
-                            save_attention_maps=config['save_attention_maps'],
-                            save_path_attention_maps = os.path.join(ATTN_SAVE_DIR, f"seg_blur_sigma-{seg_blur_sigma}_seg_scale-{seg_scale}_guidance_scale-{guidance_scale}")\
-                                                        if config['save_attention_maps'] else None ,
-                            sample_ct_attn_maps = config['sample_ct_attn_maps']
-
-                        ).images
-            save_path = os.path.join(PICS_SAVE_DIR, f"guidance_scales-{guidance_scale}.png")
-            create_plot(outputs, titles,  rows=len(config['seg_scales']),
-                        cols=len(config['seg_blur_sigmas']), save_path=save_path)
+        output = pipe(
+                [prompt],
+                num_inference_steps=config['num_inference_steps'],
+                guidance_scale=config['guidance_scale'],
+                should_log_metrics = config['should_log_metrics'],
+                metric_tracked_block = config['metric_tracked_block'],
+                metric_save_dir = os.path.join(config['metric_save_dir'], name) ,
+                seg_scale=config['seg_scale'],
+                seg_applied_layers=config['seg_applied_layers'],
+                blur_time_regions=config['blur_time_regions'],
+                blurring_technique = config['blurring_technique'],
+                generator=generator,
+                sample_ct_attn_maps=config['sample_ct_attn_maps'],
+                save_attention_maps=config['save_attention_maps'],
+                save_path_attention_maps = os.path.join(config['atten_save_dir'], name)
+            ).images
+        
+        
+        save_path = os.path.join(config['pics_save_dir'], f"{name}.png")
+        
+        # save the image
+        plt.imsave(save_path, output[0])
 
