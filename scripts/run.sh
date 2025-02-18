452f9225d26364a8e5553deec764a3fbfa7c6672
Sar2580P <sporwal1818@gmail.com>
2025-02-18 21:55:24 +0530

    completed various seed runs

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Define the variables
-seeds=(77 10 769)     #  1828499611299255970 97
+seeds=(77 10 769)     # 77
 blur_time_regions=("begin" "begin mid")  # "mid" "end" "begin mid end"
 seg_applied_layers=("mid")  # "down" "up" "mid down" "mid up" "down up" "mid down up"
 metric_tracked_block=("mid")   #  "down" "up"
 num_inference_steps=(30)    # 20 30 40
-interpolatedBoxBlur_techniques=("interpolatedBoxBlur_5_0.9" "interpolatedBoxBlur_17_0.9" "interpolatedBoxBlur_21_0.9" "interpolatedBoxBlur_31_0.9")
+interpolatedBoxBlur_techniques=("interpolatedBoxBlur_5_0.9" "interpolatedBoxBlur_17_0.9" "interpolatedBoxBlur_21_0.9" "interpolatedBoxBlur_31_0.9" "interpolatedBoxBlur_61_0.9")
 temperatureAnnealing_techniques=("temperatureAnnealing_linear_2" "temperatureAnnealing_linear_2.4" "temperatureAnnealing_cosine_4" "temperatureAnnealing_cosine_6" "temperatureAnnealing_exponential_2.5" "temperatureAnnealing_exponential_3.3")
-gaussian_techniques=("gaussian_3_10" "gaussian_7_10" "gaussian_17_10" "gaussian_-1_10" "gaussian_-1_10000")
-ema_techniques=("ema_0.75_0.9_linear" "ema_0.85_0.95_linear" "ema_0.99_0.99_linear" "ema_0.85_0.99_quadratic" "ema_0.9_0.99_quadratic" "ema_0.9_0.99_cosine" "ema_0.8_0.95_cosine")
-blurring_technique=("${interpolatedBoxBlur_techniques[@]}")
+# gaussian_techniques=("gaussian_3_10" "gaussian_7_10" "gaussian_17_10" "gaussian_-1_10" "gaussian_-1_10000")
+ema_techniques=("ema_0.85_0.9_linear" "ema_0.95_0.99_linear" "ema_0.50_0.65_linear" "ema_0.6_0.75_linear" "ema_0.75_0.99_linear")
+gaussian_techniques=("gaussian_31_10")
+# blurring_technique=("${interpolatedBoxBlur_techniques[@]}" "${gaussian_techniques[@]}" "${ema_techniques[@]}")
+blurring_technique=("${gaussian_techniques[@]}")
 guidance_scale=(0 5)
 seg_scale=(0 3)
 
